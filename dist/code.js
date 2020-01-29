364ccafa748f64d5b321b8e74eee1504e82107b9
Helen Cheung <helenkei.cheung@gmail.com>
2020-01-29 05:54:19 -0800

    tweaked mouseover tooltip abs location

--- dist/code.js
+++ dist/code.js
@@ -106,8 +106,8 @@ const d3Mapping = (year, type) => (
                         .duration(200)
                         .style('opacity', .9);
                     div.html(d.properties.name + "<br/>" + 'Rate: ' + d.properties.value)
-                        .style('left', (d3.event.pageX) + 'px')
-                        .style('top', (d3.event.pageY - 28) + 'px');
+                        .style('left', (event.clientX - 225) + 'px')
+                        .style('top', (event.clientY - 30) + 'px');
                 })
                 .on('mouseout', function (d) {
                     div.transition()
